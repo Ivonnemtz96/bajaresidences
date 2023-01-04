33d1200e52f383978af7bdb365dcad3c01db4f65
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-04 15:09:22 -0700

    Info contacto Ok, propiedades Ok

--- modules/contacto.php
+++ modules/contacto.php
@@ -1,13 +1,13 @@
 <div class="page-title" style="background-image: url(/assets/images/background/page-title-6.jpg);">
     <div class="auto-container">
-        <h1>Contact Us</h1>
+        <h1>Contáctanos</h1>
     </div>
 </div>
 <div class="bredcrumb-wrap">
     <div class="auto-container">
         <ul class="bredcrumb-list">
-            <li><a href="index.html">Home</a></li>
-            <li>Contact Us</li>
+            <li><a href="/">Inicio</a></li>
+            <li>Contáctanos</li>
         </ul>
     </div>
 </div>
@@ -16,21 +16,21 @@
 <section class="section-padding ">
     <div class="auto-container">
         <div class="section_heading mb_40">
-            <span class="section_heading_title_small">Our location</span>
-            <h2 class="section_heading_title_big">Explore Our Office <br> Worldwide</h2>
+            <span class="section_heading_title_small">Información de contacto</span>
+            <h2 class="section_heading_title_big">Si tienes alguna duda o pregunta<br> Estamos para servirte </h2>
         </div>
         <ul class="contact-info-1 d-flex flex-wrap mb_40">
             <li>
-                <h4 class="fs_20 mb_10"><i class="icon-27 mr_10 fs_18"></i>Location</h4>
-                <p>New Hyde Park, NY 11040</p>
+                <h4 class="fs_20 mb_10"><i class="icon-27 mr_10 fs_18"></i>Dirección</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </li>
             <li>
-                <h4 class="fs_20 mb_10"><i class="icon-28 mr_10 fs_18"></i>Phone No</h4>
-                <p><a href="tel:(+91) 333 666 0021">(+91) 333 666 0021</a></p>
+                <h4 class="fs_20 mb_10"><i class="icon-28 mr_10 fs_18"></i>Teléfono</h4>
+                <p><a href="tel:+521234567890">+52 123 456 7890</a></p>
             </li>
             <li>
-                <h4 class="fs_20 mb_10"><i class="icon-29 mr_10 fs_15"></i>Email</h4>
-                <p><a href="mailto:support@example.com">support@example.com</a></p>
+                <h4 class="fs_20 mb_10"><i class="icon-29 mr_10 fs_15"></i>Correo</h4>
+                <p><a href="mailto:info@bajaresidence.com">info@bajaresidence.com</a></p>
             </li>
         </ul>
         <div class="row no-gutters">
@@ -39,25 +39,26 @@
                     <form method="post" id="contact-form">
                         <div class="row">
                             <div class="form-group col-md-12">
-                                <input type="text" name="form_name" value="" placeholder="Your Name" required>
+                                <input type="text" name="form_name" value="" placeholder="Nombre *" required>
                             </div>
 
                             <div class="form-group col-md-12">
-                                <input type="text" name="email" value="" placeholder="Your Email" required>
+                                <input type="text" name="email" value="" placeholder="Correo *" required>
                             </div>
 
                             <div class="form-group col-md-12">
-                                <input type="text" name="form_subject" value="" placeholder="Subject" required>
+                                <input type="text" name="form_subject" value="" placeholder="Asunto *" required>
                             </div>
 
                             <div class="form-group col-md-12">
-                                <textarea name="form_message" placeholder="Massage"></textarea>
+                                <textarea name="form_message" placeholder="Mensaje *"></textarea>
                             </div>
                             <div class="form-group col-md-12">
                                 <input id="form_botcheck" name="form_botcheck" class="form-control" type="hidden"
                                     value="">
-                                <button class="btn-1" type="submit" data-loading-text="Please wait...">Submit Now <i
-                                        class="flaticon-right-arrow-1"></i><span></span></button>
+                                <button class="btn-1" type="submit" data-loading-text="Please wait...">
+                                    Enviar <i class="flaticon-right-arrow-1"></i><span></span>
+                                </button>
                             </div>
                         </div>
                     </form>
@@ -65,10 +66,7 @@
             </div>
             <div class="col-lg-7">
                 <div class="map mb_30">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55945.16225505631!2d-73.90847969206546!3d40.66490264739892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1601263396347!5m2!1sen!2sbd"
-                        width="600" height="535" frameborder="0" style="border:0; width: 100%" allowfullscreen=""
-                        aria-hidden="false" tabindex="0"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1959.615815859099!2d-109.70890221056162!3d23.090307280535058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af51532c7132c7%3A0xf09c9724da511bbd!2sBanana%20Marketing%20Group!5e0!3m2!1ses-419!2smx!4v1672868864227!5m2!1ses-419!2smx" width="700" height="535" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
--- modules/propiedades.php
+++ modules/propiedades.php
@@ -1,6 +1,24 @@
-<!-- Room -->
+<div class="page-title" style="background-image: url(/assets/images/background/page-title-5.jpg);">
+        <div class="auto-container">
+            <h1>¡Encontremos la casa de sus sueños!</h1>
+        </div>
+    </div>
+    <div class="bredcrumb-wrap">
+        <div class="auto-container">
+            <ul class="bredcrumb-list">
+                <li><a href="/">Inicio</a></li>
+                <li>Propiedades</li>
+            </ul>
+        </div>
+    </div>
+
+
 <section class="section-padding">
     <div class="auto-container">
+        <div class="section_heading text-center mb_55">
+            <span class="section_heading_title_small">Descubre</span>
+            <h2 class="section_heading_title_big">Los departamentos que tenemos para ti</h2>
+        </div>
         <div class="row">
             <div class="col-lg-4 col-md-6">
                 <div class="room-1-block wow fadeInUp" data-wow-delay=".2s" data-wow-duration=".8s">
@@ -8,19 +26,19 @@
                         <img src="/assets/images/resource/room-1.jpg" alt="">
                     </div>
                     <div class="room-1-content">
-                        <p class="room-1-meta-info">Start from <span class="theme-color">$55.0</span>/night</p>
+                        <p class="room-1-meta-info"> <span class="theme-color">160 m2</span></p>
                         <div class="room-1-rating">
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
-                            <i class="icon-7"></i>
+                            <i class="icon-6"></i>
                         </div>
-                        <h4 class="room-1-title mb_20"><a href="room-details.html">Luxury Room</a></h4>
-                        <p class="room-1-text mb_30">Mattis cras magna morb punar aenean aliquet in. Risus a arcu eget.
+                        <h4 class="room-1-title mb_20"><a href="/contacto">Desarrollo 1</a></h4>
+                        <p class="room-1-text mb_30">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, eaque?
                         </p>
-                        <div class="link-btn"><a href="room-details.html" class="btn-1 btn-alt">Book Now
-                                <span></span></a></div>
+                        <div class="link-btn"><a href="/contacto" class="btn-1 btn-alt">Contactar<span></span></a></div>
                     </div>
                 </div>
             </div>
@@ -30,19 +48,19 @@
                         <img src="/assets/images/resource/room-2.jpg" alt="">
                     </div>
                     <div class="room-1-content">
-                        <p class="room-1-meta-info">Start from <span class="theme-color">$55.0</span>/night</p>
+                        <p class="room-1-meta-info"> <span class="theme-color">120 m2</span></p>
                         <div class="room-1-rating">
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
-                            <i class="icon-7"></i>
+                            <i class="icon-6"></i>
                         </div>
-                        <h4 class="room-1-title mb_20"><a href="room-details.html">Delux Room</a></h4>
-                        <p class="room-1-text mb_30">Mattis cras magna morb punar aenean aliquet in. Risus a arcu eget.
+                        <h4 class="room-1-title mb_20"><a href="/contacto">Desarrollo 2</a></h4>
+                        <p class="room-1-text mb_30">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur?
                         </p>
-                        <div class="link-btn"><a href="room-details.html" class="btn-1 btn-alt">Book Now
-                                <span></span></a></div>
+                        <div class="link-btn"><a href="/contacto" class="btn-1 btn-alt">Contactar<span></span></a></div>
                     </div>
                 </div>
             </div>
@@ -52,85 +70,88 @@
                         <img src="/assets/images/resource/room-3.jpg" alt="">
                     </div>
                     <div class="room-1-content">
-                        <p class="room-1-meta-info">Start from <span class="theme-color">$55.0</span>/night</p>
+                        <p class="room-1-meta-info"> <span class="theme-color">180 m2</span></p>
                         <div class="room-1-rating">
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
-                            <i class="icon-7"></i>
+                            <i class="icon-6"></i>
                         </div>
-                        <h4 class="room-1-title mb_20"><a href="room-details.html">Luxury Room</a></h4>
-                        <p class="room-1-text mb_30">Mattis cras magna morb punar aenean aliquet in. Risus a arcu eget.
+                        <h4 class="room-1-title mb_20"><a href="/contacto">Desarrollo 3</a></h4>
+                        <p class="room-1-text mb_30">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, nobis.
                         </p>
-                        <div class="link-btn"><a href="room-details.html" class="btn-1 btn-alt">Book Now
-                                <span></span></a></div>
+                        <div class="link-btn"><a href="/contacto" class="btn-1 btn-alt">Contactar<span></span></a></div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="room-1-block wow fadeInUp" data-wow-delay=".2s" data-wow-duration=".8s">
-                    <div class="room-1-image hvr-img-zoom-1">
-                        <img src="/assets/images/resource/room-11.jpg" alt="">
-                    </div>
-                    <div class="room-1-content">
-                        <p class="room-1-meta-info">Start from <span class="theme-color">$55.0</span>/night</p>
-                        <div class="room-1-rating">
-                            <i class="icon-6"></i>
-                            <i class="icon-6"></i>
-                            <i class="icon-6"></i>
-                            <i class="icon-6"></i>
-                            <i class="icon-7"></i>
-                        </div>
-                        <h4 class="room-1-title mb_20"><a href="room-details.html">Luxury Room</a></h4>
-                        <p class="room-1-text mb_30">Mattis cras magna morb punar aenean aliquet in. Risus a arcu eget.
-                        </p>
-                        <div class="link-btn"><a href="room-details.html" class="btn-1 btn-alt">Book Now
-                                <span></span></a></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="room-1-block wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1.2s">
-                    <div class="room-1-image hvr-img-zoom-1">
-                        <img src="/assets/images/resource/room-12.jpg" alt="">
-                    </div>
-                    <div class="room-1-content">
-                        <p class="room-1-meta-info">Start from <span class="theme-color">$55.0</span>/night</p>
-                        <div class="room-1-rating">
-                            <i class="icon-6"></i>
-                            <i class="icon-6"></i>
-                            <i class="icon-6"></i>
-                            <i class="icon-6"></i>
-                            <i class="icon-7"></i>
-                        </div>
-                        <h4 class="room-1-title mb_20"><a href="room-details.html">Delux Room</a></h4>
-                        <p class="room-1-text mb_30">Mattis cras magna morb punar aenean aliquet in. Risus a arcu eget.
-                        </p>
-                        <div class="link-btn"><a href="room-details.html" class="btn-1 btn-alt">Book Now
-                                <span></span></a></div>
-                    </div>
-                </div>
-            </div>
+
             <div class="col-lg-4 col-md-6">
                 <div class="room-1-block wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1.5s">
                     <div class="room-1-image hvr-img-zoom-1">
-                        <img src="/assets/images/resource/room-13.jpg" alt="">
+                        <img src="/assets/images/resource/room-3.jpg" alt="">
                     </div>
                     <div class="room-1-content">
-                        <p class="room-1-meta-info">Start from <span class="theme-color">$55.0</span>/night</p>
+                        <p class="room-1-meta-info"> <span class="theme-color">180 m2</span></p>
                         <div class="room-1-rating">
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
-                            <i class="icon-7"></i>
+                            <i class="icon-6"></i>
                         </div>
-                        <h4 class="room-1-title mb_20"><a href="room-details.html">Luxury Room</a></h4>
-                        <p class="room-1-text mb_30">Mattis cras magna morb punar aenean aliquet in. Risus a arcu eget.
+                        <h4 class="room-1-title mb_20"><a href="/contacto">Desarrollo 4</a></h4>
+                        <p class="room-1-text mb_30">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, nobis.
                         </p>
-                        <div class="link-btn"><a href="room-details.html" class="btn-1 btn-alt">Book Now
-                                <span></span></a></div>
+                        <div class="link-btn"><a href="/contacto" class="btn-1 btn-alt">Contactar<span></span></a></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="room-1-block wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1.5s">
+                    <div class="room-1-image hvr-img-zoom-1">
+                        <img src="/assets/images/resource/room-3.jpg" alt="">
+                    </div>
+                    <div class="room-1-content">
+                        <p class="room-1-meta-info"> <span class="theme-color">180 m2</span></p>
+                        <div class="room-1-rating">
+                            <i class="icon-6"></i>
+                            <i class="icon-6"></i>
+                            <i class="icon-6"></i>
+                            <i class="icon-6"></i>
+                            <i class="icon-6"></i>
+                        </div>
+                        <h4 class="room-1-title mb_20"><a href="/contacto">Desarrollo 5</a></h4>
+                        <p class="room-1-text mb_30">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, nobis.
+                        </p>
+                        <div class="link-btn"><a href="/contacto" class="btn-1 btn-alt">Contactar<span></span></a></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="room-1-block wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1.5s">
+                    <div class="room-1-image hvr-img-zoom-1">
+                        <img src="/assets/images/resource/room-3.jpg" alt="">
+                    </div>
+                    <div class="room-1-content">
+                        <p class="room-1-meta-info"> <span class="theme-color">180 m2</span></p>
+                        <div class="room-1-rating">
+                            <i class="icon-6"></i>
+                            <i class="icon-6"></i>
+                            <i class="icon-6"></i>
+                            <i class="icon-6"></i>
+                            <i class="icon-6"></i>
+                        </div>
+                        <h4 class="room-1-title mb_20"><a href="/contacto">Desarrollo 6</a></h4>
+                        <p class="room-1-text mb_30">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, nobis.
+                        </p>
+                        <div class="link-btn"><a href="/contacto" class="btn-1 btn-alt">Contactar<span></span></a></div>
                     </div>
                 </div>
             </div>
