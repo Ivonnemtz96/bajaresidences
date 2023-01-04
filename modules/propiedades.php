e44685c01807b0686fda40315a1a914068746aaf
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-04 16:39:01 -0700

    Cambio de banner de titulo img

--- modules/propiedades.php
+++ modules/propiedades.php
@@ -113,10 +113,10 @@
             <div class="col-lg-4 col-md-6">
                 <div class="room-1-block wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1.5s">
                     <div class="room-1-image hvr-img-zoom-1">
-                        <img src="/assets/images/resource/room-3.jpg" alt="">
+                        <img src="/assets/images/resource/room-2.jpg" alt="">
                     </div>
                     <div class="room-1-content">
-                        <p class="room-1-meta-info"> <span class="theme-color">180 m2</span></p>
+                        <p class="room-1-meta-info"> <span class="theme-color">150 m2</span></p>
                         <div class="room-1-rating">
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
@@ -136,10 +136,10 @@
             <div class="col-lg-4 col-md-6">
                 <div class="room-1-block wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1.5s">
                     <div class="room-1-image hvr-img-zoom-1">
-                        <img src="/assets/images/resource/room-3.jpg" alt="">
+                        <img src="/assets/images/resource/room-1.jpg" alt="">
                     </div>
                     <div class="room-1-content">
-                        <p class="room-1-meta-info"> <span class="theme-color">180 m2</span></p>
+                        <p class="room-1-meta-info"> <span class="theme-color">130 m2</span></p>
                         <div class="room-1-rating">
                             <i class="icon-6"></i>
                             <i class="icon-6"></i>
